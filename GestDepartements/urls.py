c0b4409568549b059d9c1671d7b7908f9cd47a73
Miradi <mengimiradit@gmail.com>
2026-05-02 03:15:13 +0200

    référencement seo

--- GestDepartements/urls.py
+++ GestDepartements/urls.py
@@ -3,11 +3,16 @@ from . import views
 urlpatterns = [
     path('',views.Accueil, name="accueil"),
     path('Apropos/', views.Apropos, name='apropos'),
-    path('Departements/<slug:slug>/', views.departements, name='departements'),
-    path('Méditation/<slug:slug>/', views.meditations, name='meditations'),
-    path('Activites/<slug:slug>/', views.activites, name='activites'),
-    path('Interventions/<slug:slug>/', views.Interventions, name='Interventions'),
-    path('Membres/<slug:slug>/', views.Members, name='Membres'),
+    path('Departements/<slug:slug>/', views.departements, name='liste_departements'),
+    path('Departements/', views.departements, name='departements'),
+    path('Méditation/<slug:slug>/', views.meditations, name='liste_meditations'),
+    path('Méditation/', views.meditations, name='meditations'),
+    path('Activites/', views.activites, name='activites'),
+    path('Activites/<slug:slug>/', views.activites, name='liste_activites'),
+    path('Interventions/<slug:slug>/', views.Interventions, name='liste_Interventions'),
+    path('Interventions/', views.Interventions, name='Interventions'),
+    path('Membres/<slug:slug>/', views.Members, name='liste_Membres'),
+    path('Membres/', views.Members, name='Membres'),
     path('Contact/', views.Contact, name='contact'),
     
 ]
