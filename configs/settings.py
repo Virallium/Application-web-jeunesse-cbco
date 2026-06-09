1832010952e833251e3a9daab29d3eb52b115963
Virallium <mengimiradit@gmail.com>
2026-06-09 21:23:22 +0200

    fghjk

--- configs/settings.py
+++ configs/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l@()t#y4a$e*$r3j+md2zury=kpaec(*kpqnfb&2jvvebkl18c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
